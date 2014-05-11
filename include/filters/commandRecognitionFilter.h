1c8fd99bed95f07d2692803625cf136a5e4f2d50
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2014-05-11 14:34:24 +0400

    add signal command() to the commandRecognition

--- include/filters/commandRecognitionFilter.h
+++ include/filters/commandRecognitionFilter.h
@@ -16,7 +16,7 @@ namespace triksound {
 /**
  * @brief The CommandRecognitionFilter class provides AudioFilter interface for Pocketsphinx decoder.
  *        This filter has input, but hasn't audio output.
- *        Instead, filter puts results of recognition to text stream defined by user of this class.
+ *        Instead, filter sends results of recognition in the signal.
  */
 class CommandRecognitionFilter : public AudioFilter
 {
@@ -24,34 +24,28 @@ class CommandRecognitionFilter : public AudioFilter
 public:
 	/**
 	 * @param decoder Pointer to decoder object. If NULL is passed, new one will be created.
-	 * @param stream Pointer to the text stream for text output. By default, this is stdout.
 	 */
 	explicit CommandRecognitionFilter(const QSharedPointer<PocketsphinxDecoder>& decoder
 										= QSharedPointer<PocketsphinxDecoder>(NULL),
-									  const QSharedPointer<QTextStream>& stream
-										= QSharedPointer<QTextStream>(NULL),
 									  QObject *parent = 0);
 
 public slots:
 	void input(AudioBuffer buf);
 
+signals:
+	void command(QString cmd);
+
 private:
 	QSharedPointer<PocketsphinxDecoder> mDecoder;
-	QSharedPointer<QTextStream> mStream;
 };
 
 inline CommandRecognitionFilter::CommandRecognitionFilter(const QSharedPointer<PocketsphinxDecoder>& decoder,
-												   const QSharedPointer<QTextStream>& stream,
 												   QObject* parent):
 	AudioFilter(parent),
-	mDecoder(decoder),
-	mStream(stream)
+	mDecoder(decoder)
 {
 	if (!mDecoder) {
 		mDecoder = QSharedPointer<PocketsphinxDecoder>(new PocketsphinxDecoder());
-	}
-	if (!mStream) {
-		mStream = QSharedPointer<QTextStream>(new QTextStream(stdout));
 	}
 }
 
@@ -60,14 +54,7 @@ inline void CommandRecognitionFilter::input(AudioBuffer buf)
 	clock_t c1 = clock();
 	QString cmd = mDecoder->recognize(buf).getText();
 	clock_t c2 = clock();
-//	qDebug() << "PS time = " << (c2 - c1) / (CLOCKS_PER_SEC / 1000);
-	if (cmd == "") {
-		(*mStream) << "\n Unrecognized command \n";
-	}
-	else {
-		(*mStream) << "\n" << cmd << "\n";
-	}
-	mStream->flush();
+	emit command(cmd);
 }
 
 }
